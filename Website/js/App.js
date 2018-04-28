b45ab14aaf120e4fa098c490177ded7a3a4ed466
Katherine Suazo <katherine@Katherines-MacBook-Air.local>
2018-04-28 01:24:22 -0400

    Linking HTML to the JS file

--- Website/js/App.js
+++ Website/js/App.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-   res.send("Hello world!");
+   res.sendFile('/Users/katherine/Desktop/NYCProject/Website/index.html');
 });
 
 app.listen(3000);
