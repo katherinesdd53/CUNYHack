0c8a01436f2c7c152a50f8a0597f0bd4222f1778
cryptic20 <sherwin.wyco20@gmail.com>
2018-04-28 03:38:54 -0400

    upgraded watson

--- server.js
+++ server.js
@@ -1,51 +1,16 @@
-var express = require('express');
-var app     = express();
-var path    = require("path");
 
-app.use(express.static('static_files'));
+'use strict';
 
-app.listen(process.env.PORT || 3000, function(){
-	console.log('listening to port 3000');
-});
 
- app.get('/',function(req, res){
- 	res.sendFile(__dirname + '/index.html');
+var server = require('./app');
+var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
 
- });
+server.listen(port, function() {
+  // eslint-disable-next-line
+  console.log('Server running on port: %d', port);
 
-app.get('*', function(req,res){
-	res.status(404).send("page doesn't exist!");
 
 });
 
-//watson api
-var ConversationV1 = require('watson-developer-cloud/conversation/v1');
 
-// Set up Conversation service wrapper.
-var conversation = new ConversationV1({
-	version_date: '2018-02-16',
-    username: '761e9ce2-2abe-4b50-9590-7789bef68fc5',
-    password: 'kWtvnkcWhUGW',
-    "url": "https://gateway.watsonplatform.net/assistant/api"
 
-});
-
-var workspace_id = 'abf43946-7561-445f-87a4-992283c59294'; // replace with workspace ID
-
-// Start conversation with empty message.
-conversation.message({
-  workspace_id: workspace_id
-  }, processResponse);
-
-// Process the conversation response.
-function processResponse(err, response) {
-  if (err) {
-    console.error(err); // something went wrong
-    return;
-  }
-
-  // Display the output from dialog, if any.
-  if (response.output.text.length != 0) {
-      console.log(response.output.text[0]);
-  }
-}
